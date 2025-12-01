be3e1adbedf40d86a0b9ada4067517d7df3cfd53
Thanya <thanyas125@gmail.com>
2025-12-01 08:38:41 -0800

    Mobile fix - remove async

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "46b151cc085da145df150e691d2b256f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "9a641e86c008fd3683624cd3754dcc6c",
+"flutter_bootstrap.js": "bffdccbf7f5a5d2f0b41d1b029dd95d5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
